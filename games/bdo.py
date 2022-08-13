f7508a33f4d945471ef5362a77e5be71dec56acf
avv77 <mag7777@mail.ru>
2022-08-13 20:26:09 +0300

    Написал тест на bdo. Разбил функцию bdo на несколько функций.

--- games/bdo.py
+++ games/bdo.py
@@ -5,11 +5,14 @@ import sqlite3
 from setting import db
 
 
-def bdo_online():
+def bdo_soup():
     url = 'https://www.ru.playblackdesert.com/News/Notice'
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
+    return soup
 
+
+def bdo_links_date_title(soup):
     links = [link['href'] for link in soup.find('div', {'class': 'thumb_nail_area'}).findAll('a')]
     date_news = [date.text for date in soup.find('div', {'class': 'thumb_nail_area'}).findAll(class_='date')]
     title_no_format = [title.text for title in soup.find('div', {'class': 'thumb_nail_area'}).findAll(class_='title')]
@@ -23,20 +26,34 @@ def bdo_online():
             if len(j) != 0:
                 j = j[:-4]
                 title_news.append(j)
+    return date_news, title_news, links
 
+
+def bdo_now_data():
     data_now = datetime.now()
     data_now_format = data_now.strftime("%d/%m/%Y")
     data_telegram = data_now.strftime("%d.%m.%Y")
+    return data_now_format, data_telegram
 
+
+def news_text_all_bdo(index, title_news, links, data_telegram):
+    title = title_news[index]
+    link = links[index]
+    news_text_all = '<b>Black Desert</b>' + '\n' + data_telegram + '\n' + f'<b>{title}</b>' + '\n' + link
+    return news_text_all
+
+
+def data_list_bdo_online(date_news, data_now_format, title_news, links, data_telegram):
     news_data_list_bdo_online = []
     for index in range(len(date_news)):
         date = date_news[index]
         if data_now_format == date:
-            title = title_news[index]
-            link = links[index]
-            news_text_all = '<b>Black Desert</b>' + '\n' + data_telegram + '\n' + f'<b>{title}</b>' + '\n' + link
+
+            news_text_all = news_text_all_bdo(index, title_news, links, data_telegram)
+
             conn = sqlite3.connect(db)
             cur = conn.cursor()
+
             cur.execute(f"SELECT Text FROM bdo WHERE Date = '{date}'")
             results = cur.fetchall()
             news_text_list = []
@@ -53,4 +70,20 @@ def bdo_online():
                 news_data_list_bdo_online.append(news_text_all)
                 cur.execute(f"INSERT INTO 'bdo' VALUES('{date}','{news_text_all}')")
                 conn.commit()
+    return news_data_list_bdo_online
+
+
+def bdo_online():
+
+    soup = bdo_soup()
+
+    date_news = bdo_links_date_title(soup)[0]
+    title_news = bdo_links_date_title(soup)[1]
+    links = bdo_links_date_title(soup)[2]
+
+    data_now_format = bdo_now_data()[0]
+    data_telegram = bdo_now_data()[1]
+
+    news_data_list_bdo_online = data_list_bdo_online(date_news, data_now_format, title_news, links, data_telegram)
+
     return news_data_list_bdo_online
---
+++ tests/test_bdo.py
@@ -0,0 +1,31 @@
+import unittest
+from datetime import datetime
+from games.bdo import bdo_soup, bdo_links_date_title, bdo_now_data, news_text_all_bdo
+import requests
+from bs4 import BeautifulSoup
+
+
+class Bdo(unittest.TestCase):
+
+    def test_bdo_soup(self):
+        self.assertTrue(bdo_soup())
+
+    def test_bdo_links_date_title(self):
+        url = 'https://www.ru.playblackdesert.com/News/Notice'
+        response = requests.get(url)
+        soup = BeautifulSoup(response.content, 'html.parser')
+        self.assertTrue(bdo_links_date_title(soup))
+
+    def test_bdo_now_data(self):
+        data_now_test = datetime.now()
+        data_now_format = data_now_test.strftime("%d/%m/%Y")
+        test = bdo_now_data()
+        self.assertEqual(data_now_format, test[0])
+
+    def test_news_text_all_bdo(self):
+        index = 0
+        title_news = ['Новость']
+        links = [r'https://1.ru']
+        data_telegram = '13.08.2022'
+        news_test = news_text_all_bdo(index, title_news, links, data_telegram)
+        self.assertEqual(news_test, '<b>Black Desert</b>\n13.08.2022\n<b>Новость</b>\nhttps://1.ru')
