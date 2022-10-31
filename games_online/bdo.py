24aa345390cf66f5778f8862de57e116dac3bb7d
avv77 <mag7777@mail.ru>
2022-10-31 16:26:08 +0300

    Исправил в bdo выгрузку soup с request на альтернативу.

--- games_online/bdo.py
+++ games_online/bdo.py
@@ -1,14 +1,22 @@
 from datetime import datetime
 from bs4 import BeautifulSoup
-import requests
 import sqlite3
 from setting import db
+from selenium import webdriver
+import os
 
 
 def bdo_soup():
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+
     url = 'https://www.ru.playblackdesert.com/News/Notice'
-    response = requests.get(url)
-    soup = BeautifulSoup(response.content, 'html.parser')
+    driver.get(url)
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
     return soup
 
 
